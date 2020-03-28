6b827f9e713dc4f13c76dc25e0f60cbb08eed240
JosephRVara <josephroccovara@gmail.com>
2020-03-28 17:05:58 -0400

    changes

--- getInTheFlask/hello-flask.py
+++ getInTheFlask/hello-flask.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Meredith.....I LOVE YOU!!"
 
 if __name__ == "__main__":
-    app.run(host='137.140.142.34', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
